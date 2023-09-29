3e87c9037df8a9d385c45e1730f5c44b1410726b
leann may nagangga <nagangga.leann690@gmail.com>
2023-09-29 19:05:07 +0800

    First commit

---
+++ quiz1.py
@@ -0,0 +1,11 @@
+input_str = input("how much liters would you want sir?")
+#value of the price in liters
+price = 70
+liters = 5
+liters = float(input_str)
+
+#calculate the total
+total = liters *70
+
+#display the total
+print(f"the total cost is:{total}")
